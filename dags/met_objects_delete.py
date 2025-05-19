06d6480bca35d846037e1c9b795857cbdf437c37
Felipe Lambert <felipe.lambert@outlook.com>
2025-05-19 15:45:49 -0300

    Add new DAG for deleting records from ClickHouse: implement met_museum_to_clickhouse_dynamic

---
+++ dags/met_objects_delete.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.providers.clickhouse.operators.clickhouse import ClickHouseOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 5, 19),
+    'retries': 1,
+}
+
+dag_id = 'met_museum_to_clickhouse_dynamic'
+
+def create_dag(dag_id, objectIDs=None, batch_size=10, **kwargs):
+    dag = DAG(
+        dag_id,
+        default_args=default_args,
+        schedule_interval=None,  # Set to None to only run manually
+        catchup=False,
+        tags=['example'],
+        **kwargs
+    )
+
+    # Task to delete records from ClickHouse table
+    delete_records = ClickHouseOperator(
+        task_id='delete_records',
+        database='default',
+        sql="TRUNCATE TABLE IF EXISTS met_objects",
+        clickhouse_conn_id='clickhouse_default',
+        dag=dag,
+    )
+
+    # Dummy task to represent the start of the DAG
+    start = DummyOperator(
+        task_id='start',
+        dag=dag,
+    )
+
+    # Define other tasks here...
+
+    # Set task dependencies
+    start >> delete_records
+
+    return dag
+
+# Create the DAG instance
+dag_instance = create_dag(dag_id)
